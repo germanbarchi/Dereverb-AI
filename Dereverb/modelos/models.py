843e9281fe25d168a8b23117956f21ca4e8aa4ba
germanbarchi <69812320+germanbarchi@users.noreply.github.com>
2020-08-31 12:04:25 -0300

    cnn_3

--- Dereverb/modelos/models.py
+++ Dereverb/modelos/models.py
@@ -37,3 +37,17 @@ def cnn_2(N):
   out = overlap_and_add(dec,128)
   model = tf.keras.Model(inputs=[layer_in],outputs=[out])
   return model
+
+
+def cnn_3(N)
+
+  N_basis = 128
+  win_size = 512
+  hop_size = 128
+
+  audio_in = tfkl.Input(shape=(N,1))
+  frame_encoder = tfkl.Conv1D(N_basis,win_size,hop_size,use_bias=False)(audio_in)
+  frame_encoder = tf.expand_dims(frame_encoder,axis=-2)
+  frame_decoder = tfkl.Conv2DTranspose(1,(win_size,1),(hop_size,1),use_bias=False)(frame_encoder)
+  model = tf.keras.Model(inputs=[audio_in],outputs=[frame_decoder])
+  return model
